223829a0c898ac03fc8cd66b04cda7f9414a80af
Konstantin Đorđević <vomindoraan@gmail.com>
2018-06-23 02:08:19 +0200

    Use EntityManager as a singleton

--- src/student/JPAUtil.java
+++ src/student/JPAUtil.java
@@ -1,19 +1,30 @@
 package student;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtil {
 	public static final EntityManagerFactory EMF;
+	public static final EntityManager EM;
 	
 	static {
 		try {
 			EMF = Persistence.createEntityManagerFactory("DeliveryServicePU");
+			EM = EMF.createEntityManager();
 		} catch (Throwable t) {
-			System.err.println("Cannot create EntityManagerFactory");
+			System.err.println("Cannot initialize persistence context");
 			throw new ExceptionInInitializerError(t);
 		}
 	}
 	
+//	public static EntityManagerFactory getEntityManagerFactory() {
+//		return EMF;
+//	}
+//
+//	public static EntityManager getEntityManager() {
+//		return EMF.createEntityManager();
+//	}
+
 	private JPAUtil() {}
 }
