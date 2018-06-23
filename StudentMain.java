57a4cb5250c28b21fd0a890ef9f9823f259832ae
Konstantin Đorđević <vomindoraan@gmail.com>
2018-06-23 05:25:52 +0200

    Use (non-existent) implementations in StudentMain

--- StudentMain.java
+++ StudentMain.java
@@ -1,30 +1,29 @@
 import operations.*;
+import student.*;
 import tests.TestHandler;
 import tests.TestRunner;
 
-
 public class StudentMain {
+	public static void main(String[] args) {
+		CityOperations cityOperations = new dk140414_CityOperations();
+		DistrictOperations districtOperations = new dk140414_DistrictOperations();
+		CourierOperations courierOperations = new dk140414_CourierOperations();
+		CourierRequestOperation courierRequestOperation = new dk140414_CourierRequestOperation();
+		GeneralOperations generalOperations = new dk140414_GeneralOperations();
+		UserOperations userOperations = new dk140414_UserOperations();
+		VehicleOperations vehicleOperations = new dk140414_VehicleOperations();
+		PackageOperations packageOperations = new dk140414_PackageOperations();
 
-    public static void main(String[] args) {
-        CityOperations cityOperations = null; // Change this to your implementation.
-        DistrictOperations districtOperations = null; // Do it for all classes.
-        CourierOperations courierOperations = null; // e.g. = new MyDistrictOperations();
-        CourierRequestOperation courierRequestOperation = null;
-        GeneralOperations generalOperations = null;
-        UserOperations userOperations = null;
-        VehicleOperations vehicleOperations = null;
-        PackageOperations packageOperations = null;
+		TestHandler.createInstance(
+				cityOperations,
+				courierOperations,
+				courierRequestOperation,
+				districtOperations,
+				generalOperations,
+				userOperations,
+				vehicleOperations,
+				packageOperations);
 
-        TestHandler.createInstance(
-                cityOperations,
-                courierOperations,
-                courierRequestOperation,
-                districtOperations,
-                generalOperations,
-                userOperations,
-                vehicleOperations,
-                packageOperations);
-
-        TestRunner.runTests();
-    }
+		TestRunner.runTests();
+	}
 }
